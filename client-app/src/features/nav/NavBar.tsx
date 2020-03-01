ac2a095682d38d23925fcc0b2d15d63ce302db62
Bruno Rodrigues <bytebruno@gmail.com>
2020-03-01 19:19:27 -0300

    MobX implementation

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import ActivityStore from '../../app/stores/activityStore';
 
-interface IProps {
-  openCreateForm: () => void;
-}
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
 
-const NavBar: React.FC<IProps> = ({openCreateForm}) => {
   return (
     <Menu fixed='top' inverted>
     <Container>
@@ -15,7 +14,7 @@ const NavBar: React.FC<IProps> = ({openCreateForm}) => {
       </Menu.Item>
       <Menu.Item name="Activities" />
       <Menu.Item>
-          <Button onClick={openCreateForm} positive content="Create activity" />
+          <Button onClick={activityStore.openCreateForm} positive content="Create activity" />
       </Menu.Item>
     </Container>
     </Menu>
