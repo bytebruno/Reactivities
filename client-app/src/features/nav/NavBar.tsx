5b4c4873f66d4aef1c17029b1110ebb8e05d8eac
Bruno Rodrigues <bytebruno@gmail.com>
2020-03-14 21:58:00 -0300

    updating code to study on notebook

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
 import ActivityStore from '../../app/stores/activityStore';
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   const activityStore = useContext(ActivityStore);
@@ -8,13 +9,13 @@ const NavBar: React.FC = () => {
   return (
     <Menu fixed='top' inverted>
     <Container>
-      <Menu.Item header>
+      <Menu.Item header as={NavLink} exact to='/'>
         <img src="/assets/logo.png" alt="logo" style={{marginRight: 10}}/>
         Reactivities
       </Menu.Item>
-      <Menu.Item name="Activities" />
+      <Menu.Item name="Activities" as={NavLink} to='/activities' />
       <Menu.Item>
-          <Button onClick={activityStore.openCreateForm} positive content="Create activity" />
+          <Button as={NavLink} to='/createActivity' positive content="Create activity" />
       </Menu.Item>
     </Container>
     </Menu>
